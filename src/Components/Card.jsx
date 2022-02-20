b9ee2e82005dd5c43ed3b55fd35a4e1f58938935
Dale <djs93@njit.edu>
2022-02-20 17:04:49 -0500

    Images in phone mode fit properly

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../bem.css';
 /* eslint-disable-next-line object-curly-newline */
-function Card({title, desc, price, onSale}) {
+function Card({title, desc, price, onSale, imgLocation}) {
     return (
         <div className="card">
-            <div className="card__content" />
+            <div className="card__content">
+                <img src={imgLocation} alt="Card" />
+            </div>
 
             <div className="card__title_and_price">
                 <div className="card__title">
@@ -31,12 +33,14 @@ Card.propTypes = {
     desc: PropTypes.string,
     price: PropTypes.number,
     onSale: PropTypes.bool,
+    imgLocation: PropTypes.string,
 };
 Card.defaultProps = {
     title: 'world!',
     desc: '',
     price: -1,
     onSale: false,
+    imgLocation: '/images/no-image.webp',
 };
 
 export default Card;
