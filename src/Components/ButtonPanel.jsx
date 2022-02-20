75a6930c8f922ec522d8f6d4952cc77dfb0ba772
Dale <djs93@njit.edu>
2022-02-20 01:38:31 -0500

    Add ButtonPanel Component
    Based on advanced-js dropdown order

---
+++ src/Components/ButtonPanel.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../bem.css';
+
+function orderBy(sortValue, sortList) {
+    const sortListClone = [...sortList];
+    const sortedResults = (sortValue === 'title')
+        ? sortListClone.sort((a, b) => { // Strings need to be sorted in a slightly more complex way
+            const nameA = a.title.toUpperCase(); // ignore upper and lowercase
+            const nameB = b.title.toUpperCase(); // ignore upper and lowercase
+            // Sorts alphabetically.  -1 puts it before. 1 puts it after
+            if (nameA < nameB) {
+                return -1;
+            }
+            if (nameA > nameB) {
+                return 1;
+            }
+            return 0;
+        })
+        : sortListClone.sort((a, b) => { // Numbers a booleans are much simpler.
+            // Just need postive or negative number
+            // Object properties can be accessed through a string representing their name
+            return a[sortValue] - b[sortValue];
+        });
+    return sortedResults;
+}
+
+function ButtonPanel({cardDB, changeCards}) {
+    return (
+        <div id="buttonPanel">
+            <div className="form-group" style={{marginRight: '20px'}}>
+                {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                <label htmlFor="orderBy">Order By</label>
+                <select
+                    id="orderBy"
+                    name="orderBy"
+                    onChange={(event) => {
+                        changeCards(orderBy(event.target.value, cardDB));
+                    }}
+                >
+                    <option value="title">Name</option>
+                    <option value="price">Price</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+ButtonPanel.propTypes = {
+    cardDB: PropTypes.array,
+    changeCards: PropTypes.func,
+};
+ButtonPanel.defaultProps = {
+    cardDB: [],
+    changeCards: () => { console.warn('changeCard function not passed to ButtonPanel!!'); },
+};
+
+export default ButtonPanel;
