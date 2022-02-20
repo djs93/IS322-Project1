e28cd2b8a98dabeb35a46d8401e86e879391d0b5
Dale <djs93@njit.edu>
2022-02-20 01:03:18 -0500

    Combine price and title into one line

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import '../bem.css';
 
 function Card({title, desc, price}) {
     return (
         <div className="card">
             <div className="card__content" />
 
-            <div className="card__title">
-                {title}
-            </div>
+            <div className="card__title_and_price">
+                <div className="card__title">
+                    {title}
+                </div>
 
-            <div className="card__price">
-                ${price}
+                <div className="card__price">
+                    ${price}
+                </div>
             </div>
+            {desc === '' ? '' : (
+                <div className="card__body">
+                    {desc}
+                </div>
+            )}
 
-            <div className="card__body">
-                {desc}
-            </div>
         </div>
     );
 }
